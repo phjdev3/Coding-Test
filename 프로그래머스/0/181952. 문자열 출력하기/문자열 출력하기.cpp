39018f2b47278fd396d22f880010244f9db96852
박현진 <phjdev3@gmail.com>
2026-03-12 05:19:28 +0900

    [level 0] Title: 문자열 출력하기, Time: 2.19 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>     // cin, cout 
+#include <string>       // string 
+
+using namespace std;    // std:: 생략
+
+int main(void) {
+    string str;         // 문자열 변수 str 선언
+    cin >> str;         // cin으로 입력
+    cout << str;        // cout으로 출력
+    return 0;           // 정상 종료
+}
