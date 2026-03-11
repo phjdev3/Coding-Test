be904841b82f41b7df0d9acd69a0765d87a5248e
박현진 <phjdev3@gmail.com>
2026-03-12 05:24:07 +0900

    [level 0] Title: a와 b 출력하기, Time: 6.76 ms, Memory: 4.08 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "a = " << a << endl 
+         << "b = " << b << endl;   // endl은 줄바꿈
+    
+    return 0;
+}
